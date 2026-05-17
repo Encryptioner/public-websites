4ef4ddae3c9c45fd51d3e46a79775347118dc331
Ankur Mursalin <mir.ankur.ruet13@gmail.com>
2026-05-17 21:35:07 +0600

    deploy: cgpa-calculator from master@392884c

--- cgpa-calculator/assets/index-BarjUMuU.js
+++ cgpa-calculator/assets/index-BarjUMuU.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-DTekokWa.js";/*! *****************************************************************************
+import{g as Xu}from"./index-D6nuk2nH.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
